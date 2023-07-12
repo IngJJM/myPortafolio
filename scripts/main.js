23a2fe312c5f252241ea060145f83aa73f350991
Jimena <jimmy-45@hotmail.com>
2023-07-11 18:19:52 -0600

    Proyecto inicial

---
+++ scripts/main.js
@@ -0,0 +1,88 @@
+(function() {
+  "use strict";
+
+  window.addEventListener('load', () => {
+    on_page_load()
+  });
+
+  /**
+   * Se llama a la función cuando se carga la página.
+   */
+  function on_page_load() {
+    // Inicializar animaciones de desplazamiento
+    AOS.init({
+      anchorPlacement: 'top-left',
+      duration: 600,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false,
+      disable: 'mobile'
+    });
+  }
+
+  /**
+   * Efectos de barra de navegación y botones de desplazamiento al desplazarse
+   */
+  const navbar = document.getElementById('header-nav')
+  var body = document.getElementsByTagName("body")[0]
+  const scrollTop = document.getElementById('scrolltop')
+  window.onscroll = () => {
+    if (window.scrollY > 0) {
+      navbar.classList.add('fixed-top', 'shadow-sm')
+      body.style.paddingTop = navbar.offsetHeight + "px"
+      scrollTop.style.visibility = "visible";
+      scrollTop.style.opacity = 1;
+    } else {
+      navbar.classList.remove('fixed-top', 'shadow-sm')
+      body.style.paddingTop = "0px"
+      scrollTop.style.visibility = "hidden";
+      scrollTop.style.opacity = 0;
+    }
+  };
+
+ 
+  var elem = document.querySelector('.grid');
+  if(elem) {
+    imagesLoaded(elem, function() {
+      new Masonry(elem, {
+        itemSelector: '.grid-item',
+        percentPosition: true,
+        horizontalOrder: true
+      });
+    })
+  }
+
+  /**
+   * Big Picture Popup para imágenes y vídeos
+   */
+   document.querySelectorAll("[data-bigpicture]").forEach((function(e) {
+     e.addEventListener("click", (function(t){
+       t.preventDefault();
+       const data =JSON.parse(e.dataset.bigpicture)
+       BigPicture({
+        el: t.target,
+        ...data
+      })
+     })
+    )
+  }))
+
+  /**
+   * Ventana emergente de imagen grande para Photo Gallary
+   */
+   document.querySelectorAll(".bp-gallery a").forEach((function(e) {
+    var caption = e.querySelector('figcaption')
+    var img = e.querySelector('img')
+    // Establecer el vínculo presente en el elemento en el título en vista completa
+    img.dataset.caption = '<a class="link-light" target="_blank" href="' + e.href + '">' + caption.innerHTML + '</a>';
+    window.console.log(caption, img)
+     e.addEventListener("click", (function(t){
+       t.preventDefault();
+       BigPicture({
+        el: t.target,
+        gallery: '.bp-gallery',
+      })
+     })
+    )
+  }))
+})();
